25697e3bfacf3b5f466bf1a8c1d5f8c899a036c2
mattsheydwasser <sheydwas@MattsLaptop>
2023-01-26 10:40:53 -0700

    added comments to help personal understanding of the topic

--- hello.py
+++ hello.py
@@ -5,13 +5,16 @@ import os, cgitb
 import json
 
 cgitb.enable()
+
+# print all environment variables as json dict
 print('Content-Type: application/json')
 print()
 print(json.dumps(dict(os.environ), indent=2))
 
+# print the users browser settings as html
 # print('Content-Type: text/html')
 # print()
-# print(f"<p>HTTP_USER_AGENT = {os.environ} </p>")
+# print(f"<p>HTTP_USER_AGENT = {os.environ["HTTP_USER_AGENT"]} </p>")
 
 
 
--- login.py
+++ login.py
@@ -7,26 +7,28 @@ import secret
 from http.cookies import SimpleCookie
 import cgitb, cgi
 
+# initialize the cgitb and field storage for cookies
+# check if the input values are the same as the secret values
 cgitb.enable()
 s = cgi.FieldStorage()
 username = s.getfirst("username")
 password = s.getfirst("password")
 
-print(username, password)
-
 form_ok = username ==  secret.username and password == secret.password
 
-
+# initialize the cooker with the HTTP_COOKIE header
 cookie = SimpleCookie(os.environ["HTTP_COOKIE"])
 cookie_username = None
 cookie_password = None
 
+# if the fields are not empty, set them as username and password fields
 if cookie.get("username"):
     cookie_username = cookie.get("username").value
 if cookie.get("password"):
     cookie_password = cookie.get("password").value
 
-
+# check if the cookie values are the same as the secret values
+# if so, set them as the username and password
 cookie_ok = cookie_username == secret.username and cookie_password == secret.password
 
 if cookie_ok:
@@ -34,12 +36,15 @@ if cookie_ok:
     password = cookie_password
 
 
+# if the inputted values are the same as the secret values, set 
+# the cookies to hold them to allow for auto login in
 print("Content-Type: text/html")
 if form_ok:
     print(f"Set-Cookie: username = {username}")
     print(f"Set-Cookie: password = {password}")
 print()
 
+# handle correct and incorrect logins
 if not username and not password:
     print(login_page())
 elif username==secret.username and password==secret.password:
