d77bc6349b1bf3c6b9053d58887bc467f53ed92f
mattsheydwasser <sheydwas@MattsLaptop>
2023-01-25 17:56:57 -0700

    completed lab following instruction of TAs

--- hello.py
+++ hello.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
+
+
 import os, cgitb
+import json
 
 cgitb.enable()
-for each in os.environ:
-    print(os.environ[each])
+print('Content-Type: application/json')
+print()
+print(json.dumps(dict(os.environ), indent=2))
+
+# print('Content-Type: text/html')
+# print()
+# print(f"<p>HTTP_USER_AGENT = {os.environ} </p>")
+
+
 
